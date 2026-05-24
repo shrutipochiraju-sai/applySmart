40a3b0df76cfbc6451665cfe07732d393f2153c8
Sai Shruti Pochiraju <spochir1@asu.edu>
2026-05-23 20:58:34 -0700

    Added initial menu system

---
+++ main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("===== ApplySmart =====");
+        System.out.println("1. Add Application");
+        System.out.println("2. View Applications");
+        System.out.println("3. Exit");
+    }
+}
